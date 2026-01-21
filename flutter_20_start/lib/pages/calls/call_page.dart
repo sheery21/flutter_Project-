7d806ad6a47f6615e866bfe4b5f5d1c65a5f85ef
sheharyar Hussian <sherypagal123@gmail.com>
2026-01-21 16:56:57 +0500

    Add reusable custom AppBar widget and refactor pages
    Introduced a new Customappbarfield widget to encapsulate AppBar logic with configurable title and profile icon. Refactored CallPage and MessagePage to use this new widget, improving code reuse and maintainability.

--- flutter_20_start/lib/pages/calls/call_page.dart
+++ flutter_20_start/lib/pages/calls/call_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_20_start/widgets/customAppBarFIeld/customAppBarField.dart';
 
 class CallPage extends StatefulWidget {
   const CallPage({super.key});
@@ -11,6 +12,7 @@ class _CallPageState extends State<CallPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const Customappbarfield(title: "Calls", showProfileIcon: false,),
       body:Center(
         child: Container(
           width: MediaQuery.of( context).size.width,
